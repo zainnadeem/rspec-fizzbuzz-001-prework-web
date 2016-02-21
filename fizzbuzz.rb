037a241945cb4c1a30d7ca580f0025e7488644a9
zainnadeem <zn.nadeem@gmail.com>
2016-02-20 20:35:24 -0500

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(int)
+	if (int % 3 == 0) && (int % 5 != 0)
+		"Fizz"
+	else if 
+		(int % 5 == 0) && (int % 3 != 0) 
+		"Buzz"
+	else if (int % 3 == 0) && (int % 5 == 0)
+		"FizzBuzz"
+	else if
+		(int % 3 != 0) && (int % 5 != 0)
+		nil
+
+end
+end
+end
+end
+end
